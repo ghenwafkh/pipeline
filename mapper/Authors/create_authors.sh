1e944af04fa25c90a31ba3596c218e1d4e92cb2b
Ghinwa <ghinwafkh99@gmail.com>
2023-05-24 12:16:06 +0200

    replacing clara

--- mapper/Authors/create_authors.sh
+++ mapper/Authors/create_authors.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-clara_path=$(pwd)
-input_Authors=$(ls $clara_path/json/Authors/)
+unknown_path=$(pwd)
+input_Authors=$(ls $unknown_path/json/Authors/)
 
 for file in $input_Authors; do
 	echo
-	echo $clara_path/json/Authors/$file
-	echo $clara_path/rdf/Authors/${file%.json}.nt
+	echo $unknown_path/json/Authors/$file
+	echo $unknown_path/rdf/Authors/${file%.json}.nt
 
     echo "[CONFIGURATION]" > config.ini
-    echo "output_file: $clara_path/rdf/Authors/${file%.json}.nt" >> config.ini
+    echo "output_file: $unknown_path/rdf/Authors/${file%.json}.nt" >> config.ini
     echo "output_format: N-TRIPLES" >> config.ini
     echo "[X5GON]" >> config.ini
-    echo "mappings: $clara_path/mapper/Authors/authors_rules.ttl" >> config.ini
-    echo "file_path: $clara_path/json/Authors/$file" >> config.ini
+    echo "mappings: $unknown_path/mapper/Authors/authors_rules.ttl" >> config.ini
+    echo "file_path: $unknown_path/json/Authors/$file" >> config.ini
 
     python3 -m morph_kgc config.ini
 
--- mapper/ER-singleton/create_ER-singleton.sh
+++ mapper/ER-singleton/create_ER-singleton.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-clara_path=$(pwd)
-input_ER=$(ls $clara_path/json/ER/)
+unknown_path=$(pwd)
+input_ER=$(ls $unknown_path/json/ER/)
 
 for file in $input_ER; do
 	echo
-	echo $clara_path/json/ER/$file
-	echo $clara_path/rdf/ER-singleton/${file%.json}.nt
+	echo $unknown_path/json/ER/$file
+	echo $unknown_path/rdf/ER-singleton/${file%.json}.nt
 
     echo "[CONFIGURATION]" > config.ini
-    echo "output_file: $clara_path/rdf/ER-singleton/${file%.json}.nt" >> config.ini
+    echo "output_file: $unknown_path/rdf/ER-singleton/${file%.json}.nt" >> config.ini
     echo "output_format: N-TRIPLES" >> config.ini
     echo "[X5GON]" >> config.ini
-    echo "mappings: $clara_path/mapper/ER-singleton/ER-singleton_rules.ttl" >> config.ini
-    echo "file_path: $clara_path/json/ER/$file" >> config.ini
+    echo "mappings: $unknown_path/mapper/ER-singleton/ER-singleton_rules.ttl" >> config.ini
+    echo "file_path: $unknown_path/json/ER/$file" >> config.ini
 
     python3 -m morph_kgc config.ini
 
